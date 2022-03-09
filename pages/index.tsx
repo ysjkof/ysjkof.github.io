5b6069133e673b99a5e087165e52e5c1a5744d64
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-09 22:21:35 +0900

    메인화면 뼈대 잡는 중

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,27 @@ import { prefix } from "../constant";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="h-screen max-w-7xl mx-auto">
       <Head>
-        <title>Github Blog | Maib</title>
+        <title>Github Blog | Main</title>
       </Head>
-      <main>
-        <h1 className="text-red-400">Main</h1>
-        <p>초기 설정 중입니다.</p>
-        <Link href="/about" as={prefix + "/about"}>
-          <a>어바웃으로</a>
+      <header className="space-x-6 font-semibold text-3xl text-gray-700">
+        <Link href="/" as={prefix + "/"}>
+          <a>메인</a>
         </Link>
+        <Link href="/about" as={prefix + "/about"}>
+          <a>어바웃</a>
+        </Link>
+      </header>
+      <main className="h-full flex justify-between">
+        <aside className="w-52 bg-slate-100"></aside>
+        <article className="w-full">
+          <h1 className="text-red-400">Main</h1>
+          <p>초기 설정 중입니다.</p>
+        </article>
+        <aside className="w-52"></aside>
       </main>
-    </>
+    </div>
   );
 };
 
